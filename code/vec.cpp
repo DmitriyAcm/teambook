4ea43a239b9729d9d49cf5c0bf517f7102cbb193
Artem Nosov <temich@yandex-team.ru>
2018-12-01 03:08:11 +0300

    Make text bigger

--- code/vec.cpp
+++ code/vec.cpp
@@ -16,40 +16,19 @@ struct Vec {
     return Vec(x, y);
   }
 };
-bool cmp(Vec a, Vec b) {
-  return a.x < b.x || (a.x == b.x && a.y < b.y);
-}
-bool cw(Vec a, Vec b, Vec c) {
-  return (b - a).cross(c - b) < 0;
-}
-bool ccw(Vec a, Vec b, Vec c) {
-  return (b - a).cross(c - b) > 0;
-}
-void convex_hull(vector<Vec> & a) {
-  if (a.size() == 1) return;
-  sort(a.begin(), a.end(), &cmp);
-  Vec p1 = a[0], p2 = a.back();
-  vector<Vec> up, down;
-  up.push_back(p1);
-  down.push_back(p1);
-  for (size_t i=1; i<a.size(); ++i) {
-    if (i==a.size()-1 || cw(p1, a[i], p2)) {
-      while (up.size()>=2
-          && !cw(up[up.size()-2], up[up.size()-1], a[i]))
-        up.pop_back();
-      up.push_back (a[i]);
-    }
-    if (i == a.size()-1 || ccw(p1, a[i], p2)) {
-      while (down.size()>=2
-          && !ccw(down[down.size()-2],
-            down[down.size()-1], a[i]))
-        down.pop_back();
-      down.push_back(a[i]);
-    }
-  }
-  a.clear();
-  for (size_t i=0; i<up.size(); ++i)
-    a.push_back(up[i]);
-  for (size_t i=down.size()-2; i>0; --i)
-    a.push_back(down[i]);
+
+// CONVEX HULL: last point == first point
+vector<Vec> convex_hull(vector<Vec> a) {
+  int n = a.size(), k = 0;
+  vector<Vec> p(n * 2);
+  sort(a.begin(), a.end());
+
+  for(int i = 0; i < n; p[k++] = a[i++])
+    while(k > 1 && (p[k - 1] - p[k - 2])
+        % (p[k - 1] - a[i]) >= 0) --k;
+  for(int i = n - 2, w = k; i >= 0; p[k++] = a[i--])
+    while(k > w && (p[k - 1] - p[k - 2])
+        % (p[k - 1] - a[i]) >= 0) --k;
+  p.resize(k);
+  return p;
 }
